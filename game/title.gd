21ca500adb921e62d091afe622779617894dbdb1
Faison <fzutavern@my.wctc.edu>
2017-11-19 11:34:23 -0600

    Make the play button switch to the test map

--- game/title.gd
+++ game/title.gd
@@ -7,3 +7,7 @@ func _on_exit_button_pressed():
 # The Quit function.
 func _quit():
 	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	get_node("/root/global").goto_scene("res://test-map.tscn")
